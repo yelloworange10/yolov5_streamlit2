399c6f581f2bbcfd5f5e6d3da2289d52c63276bd
Sato.Saiko <3221217388@qq.com>
2024-05-10 15:06:04 +0800

    1

--- FrontEnd/MyColor.py
+++ FrontEnd/MyColor.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 import torch
--- FrontEnd/main.py
+++ FrontEnd/main.py
@@ -95,6 +95,7 @@ elif option == '👁️品种识别':
         if st.button("识别！😆"):
             with st.spinner("AI正在疯狂思考... 🤯🤯🤯"):
                 # 调用YOLOv5的detect.py脚本进行推断
+                print("当前工作目录:", os.getcwd())
                 subprocess.run([
                     "python", "../detect.py",
                     "--weights", "best.pt",
